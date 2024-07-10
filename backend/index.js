eb817c0b0991fa28f441c988e5b770169b6870ef
khushi-gala <119067392+khushi-gala@users.noreply.github.com>
2024-07-11 00:01:56 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const app= express();
 app.use(express.json());
 
 app.use(cors{
-    origin: '*',
+    origin: 'https://pensieve-frontend.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true  // Enable credentials (cookies, authorization headers) cross-origin
