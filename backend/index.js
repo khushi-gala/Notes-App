6b53175fd97bb441146e2f1698c777e7c2d51ca6
khushigala <galakhushi2003@gmail.com>
2024-07-11 00:06:20 +0530

    third

--- backend/index.js
+++ backend/index.js
@@ -13,12 +13,12 @@ const app= express();
 
 app.use(express.json());
 
-app.use(cors{
+app.use(cors({
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true  // Enable credentials (cookies, authorization headers) cross-origin
-});
+}));
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING)
 .then(() => console.log('Connected to MongoDB'))
