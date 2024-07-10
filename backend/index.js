95bd589d1f35348c25b4c88bb8e050437a9edc11
khushigala <galakhushi2003@gmail.com>
2024-07-10 23:55:08 +0530

    second commit

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const app= express();
 
 app.use(express.json());
 
-app.use({
+app.use(cors{
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
