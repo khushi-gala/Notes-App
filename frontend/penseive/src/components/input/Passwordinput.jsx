65f97b170f90157feab1d24364aa1618b4d5db4e
khushi-gala <119067392+khushi-gala@users.noreply.github.com>
2024-07-21 13:40:55 +0530

    Update Passwordinput.jsx

--- frontend/penseive/src/components/input/Passwordinput.jsx
+++ frontend/penseive/src/components/input/Passwordinput.jsx
@@ -6,7 +6,7 @@ const PasswordInput= ({ value, onChange, placeholder }) => {
   const toggleShowPassword = ()=>{
     setIsShowPassword(!isShowPassword);
   }
-  return (
+  return ( 
     <div className='flex items-center bg-lightpurple text-white dark:bg-stone-900 px-5 rounded mb-3'>
       <input 
       value={value}
